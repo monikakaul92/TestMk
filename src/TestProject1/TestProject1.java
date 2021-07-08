c3f75f457735d40bb30f7d35487e93fab392a026
monikakaul05 <75671277+monikakaul05@users.noreply.github.com>
2021-07-08 23:09:38 +0530

    Monika_BaiscTestChanges

--- src/TestProject1/TestProject1.java
+++ src/TestProject1/TestProject1.java
@@ -1,10 +1,15 @@
 package TestProject1;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class TestProject1 {
 
 	public static void main(String[] args) {
-		System.out.println("Hi Monika");
-
+		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://automationbykrishna.com/");
+		driver.close();
 	}
 
 }
